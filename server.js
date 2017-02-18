c9844ffd0c0a7346af5736eb9dee16972a008a5c
Nathan Monte <nathan.monte.kh@gmail.com>
2017-02-18 12:18:16 +0000

    Changed port to 8080

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ app.set('view engine', 'html');
 //Setup for public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
 })
 
 /* GET home page. */
